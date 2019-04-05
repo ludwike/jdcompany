9dd310d73a0fa893dacce079ff266bb15c58d871
Ludwike <adam9871337@gmail.com>
2019-04-05 09:35:44 +0200

    s

---
+++ lol/enter.js
@@ -0,0 +1,5 @@
+function keyUpHandler(e)
+{
+    e = e || event;
+    alert("kod: " + e.keyCode);
+}
---
+++ lol/lol.js
@@ -0,0 +1,40 @@
+function onLoad1(){
+var wartoscnick2 = document.getElementById('nick2').value;
+document.getElementById('nazwa').innerHTML = wartoscnick2;
+if(wartoscnick2==""){
+document.getElementById('nazwa').innerHTML = 'Your_nickname';}
+}
+
+
+
+
+
+
+function przekierowanie1(){
+  var a = document.getElementById('nick').value;
+  var b = document.getElementById('nick2').value;
+if(a==0){var a = 'euw'}
+if(b != ""){window.location = ("https://" + a + ".op.gg/summoner/userName=" + b +""); return;}
+alert('Write your nickname clown!');
+}
+function przekierowanie2(){
+  var a = document.getElementById('nick').value;
+  var b = document.getElementById('nick2').value;
+if(a==0){var a = 'euw'}
+if(b != ""){window.location = ("https://wol.gg/stats/" + a +"/" + b +"/"); return;}
+alert('Write your nickname clown!');
+}
+function przekierowanie3(){
+  var a = document.getElementById('nick').value;
+  var b = document.getElementById('nick2').value;
+if(a==0){var a = 'euw'}
+if(b != ""){window.location = ("https://www.masterypoints.com/player/" + b + "/" + a + ""); return;}
+alert('Write your nickname clown!');
+}
+function przekierowanie4(){
+  var a = document.getElementById('nick').value;
+  var b = document.getElementById('nick2').value;
+if(a==0){var a = 'euw'}
+if(b != ""){window.location = ("http://www.lolskill.net/summoner/" + a + "/"+ b + ""); return;}
+alert('Write your nickname clown!');
+}
