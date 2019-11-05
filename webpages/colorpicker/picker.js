387a51b799fee11f577834baf0a6d1b7803ac0aa
Ludwike <adam9871337@gmail.com>
2019-11-05 20:34:06 +0100

    Colorpicker nie dziala

--- webpages/colorpicker/picker.js
+++ webpages/colorpicker/picker.js
@@ -4,3 +4,9 @@ function wlaczanie(){
 document.getElementById('glowny').style.display = "none";
 document.getElementById('s2').style.display = "block";
 }
+
+function zaladuj(){
+var jd = 5;
+console.log(jd);
+
+}
