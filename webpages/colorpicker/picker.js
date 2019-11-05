1ffe1d273d27fef05df57dc69a5ff776e5c0818b
Ludwike <adam9871337@gmail.com>
2019-11-05 21:43:29 +0100

    s

--- webpages/colorpicker/picker.js
+++ webpages/colorpicker/picker.js
@@ -6,7 +6,11 @@ document.getElementById('s2').style.display = "block";
 }
 
 function zaladuj(){
-var jd = 5;
-console.log(jd);
-
+var wartosc1 = document.getElementById('war1').value;
+var wartosc2 = document.getElementById('war2').value;
+var wartosc3 = document.getElementById('war3').value;
+document.getElementById(war1).style.color = red;
+document.getElementById('war1').value = null;
+document.getElementById('war2').value = null;
+document.getElementById('war3').value = null;
 }
