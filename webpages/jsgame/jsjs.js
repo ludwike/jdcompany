ba37eda299d500f1288c970fb9603b14b3e0d78d
Ludwike <adam9871337@gmail.com>
2019-11-09 18:28:40 +0100

    v0.002 prawidlowa funkcja na przejscie

--- webpages/jsgame/jsjs.js
+++ webpages/jsgame/jsjs.js
@@ -1,48 +1,53 @@
 var money = 0;
 var ileczekac = 0;
+var idznow = "glowny";
+var idzdo = null;
 setInterval("ladowaniezmiennych();",1000);
 setInterval("czekaj();",1000);
 
+// SKROTY FUNKCJI I KOMEND
+function poID(id){
+    return document.getElementById(id);}
+function konsola(text){
+    return console.log(text)}
+
+// LADOWANIE I PODSTAWOWE FUNKCJE!!!
+function czekaj(){
+  // sprawdzanie kasy numer 1
+  if(ileczekac > 0){
+    ileczekac--;
+    poID('odbierz1').innerHTML = ileczekac + "s";
+    poID('odbierz1').style = "background-color: red";
+    konsola("Pozostało: " + ileczekac + " sekund do darmowej kasy.");
+}
+  else {
+    poID('odbierz1').style = "background-color: #00bf00";
+    poID('odbierz1').innerHTML = "Odbierz";
+    konsola("Mozesz odebrac pieniadze.");}}
+
 function ladowaniezmiennych(){
   //pieniadze
-document.getElementById('pln').innerHTML = money + "$";
-console.log("Stan konta: " + money + "$");
+  poID('pln').innerHTML = money + "$";
+  konsola("Stan konta: " + money + "$");
   //odbieranie kasy numer 1 + kolorki
-document.getElementById('odbierz1').innerHTML = "Odbierz";
-document.getElementById('odbierz1').style = "background-color: #00bf00";
-
+  poID('odbierz1').innerHTML = "Odbierz";
+  poID('odbierz1').style = "background-color: #00bf00";
 }
 
+function przejscie(a){
+poID(idznow).style.display = "none";
+idznow = a;
+poID(a).style.display = "block";
+}
+
+// KOLEJNE FUNKCJE
 function darmowepln(){
 if(ileczekac == 0){
   var darmowyhajs = 10;
   money = money + darmowyhajs;
   ileczekac = 30;
-  document.getElementById('odbierz1').innerHTML = ileczekac + "s"; // ustawia insant kolor red
-  document.getElementById('odbierz1').style = "background-color: red";
+  poID('odbierz1').innerHTML = ileczekac + "s"; // ustawia insant kolor red
+  poID('odbierz1').style = "background-color: red";
   return 0;}
-  console.log("Musisz odczekać 30 sekund przed następnym poborem pieniędzy.");
-}
-
-function czekaj(){
-  // sprawdzanie kasy numer 1
-  if(ileczekac > 0){
-    ileczekac--;
-    document.getElementById('odbierz1').innerHTML = ileczekac + "s";
-    document.getElementById('odbierz1').style = "background-color: red";
-    console.log("Pozostało: " + ileczekac + " sekund do darmowej kasy.");
-}
-  else {
-    document.getElementById('odbierz1').style = "background-color: #00bf00";
-    document.getElementById('odbierz1').innerHTML = "Odbierz";
-    console.log("Mozesz odebrac pieniadze.");}}
-
-function changemenukody(){
-document.getElementById('glowny').style.display = "none";
-document.getElementById('kody').style.display = "block";
-}
-
-function changekodymenu(){
-document.getElementById('kody').style.display = "none";
-document.getElementById('glowny').style.display = "block";
+  konsola("Musisz odczekać 30 sekund przed następnym poborem pieniędzy.");
 }
