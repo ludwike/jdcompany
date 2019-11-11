6d95b40fff863cb129848f6c288765cfef7396e1
Ludwike <adam9871337@gmail.com>
2019-11-11 16:38:54 +0100

    W PEŁNI DZIAŁAJĄCY COINFLIP!!

--- webpages/jsgame/jsjs.js
+++ webpages/jsgame/jsjs.js
@@ -2,8 +2,12 @@ var money = 0;
 var ileczekac = 0;
 var idznow = "glowny";
 var idzdo = null;
-setInterval("ladowaniezmiennych();",1000);
-setInterval("czekaj();",1000);
+var obstawienie = null;
+var obstawieniekomputer = null;
+var stawka = 0;
+var teststawka = 0;
+
+setInterval("",4999);
 
 // SKROTY FUNKCJI I KOMEND
 function poID(id){
@@ -23,7 +27,7 @@ function czekaj(){
   else {
     poID('odbierz1').style = "background-color: #00bf00";
     poID('odbierz1').innerHTML = "Odbierz";
-    konsola("Mozesz odebrac pieniadze.");}}
+    konsola("Możesz odebrać pieniądze.");}}
 
 function ladowaniezmiennych(){
   //pieniadze
@@ -35,9 +39,9 @@ function ladowaniezmiennych(){
 }
 
 function przejscie(a){
-poID(idznow).style.display = "none";
-idznow = a;
-poID(a).style.display = "block";
+  poID(idznow).style.display = "none";
+  idznow = a;
+  poID(a).style.display = "block";
 }
 
 // KOLEJNE FUNKCJE
@@ -48,6 +52,119 @@ if(ileczekac == 0){
   ileczekac = 30;
   poID('odbierz1').innerHTML = ileczekac + "s"; // ustawia insant kolor red
   poID('odbierz1').style = "background-color: red";
-  return 0;}
+  return;}
   konsola("Musisz odczekać 30 sekund przed następnym poborem pieniędzy.");
+}
+
+// COINFLIP FUNKCJE
+//
+function obstawiam(a){
+  obstawienie = a;
+  konsola("Obstawiłeś: " + obstawienie + ".");
+if(a == "jx"){
+  poID("jx").style = "background-color:red";
+  poID("kx").style = "background-color:#e8e8e8";
+  obstawienie = 0;
+}
+else { //jeżeli to KX ofc.
+  poID("kx").style = "background-color:red";
+  poID("jx").style = "background-color:#e8e8e8";
+  obstawienie = 1;
+}
+}
+
+function zmianastawki(a){
+  teststawka = stawka + a;
+if(teststawka > money){
+  konsola("Nie masz wystarczająco pieniędzy, żeby obstawić: " + teststawka + "$");
+  teststawka = stawka;
+  zmianastawkiall()
+  return;
+}
+else  {
+  stawka += a;
+  teststawka = stawka;
+  konsola("Obstawiles: " + stawka + "$");
+  poID('pstawka').innerHTML = stawka + "$";}
+}
+function zmianastawkiall(){
+  stawka = money;
+  konsola("Obstawiles wszystkie swoje pieniadze czyli: " + stawka + "$");
+  poID('pstawka').innerHTML = stawka + "$";
+}
+function resetstawki(){
+  stawka = 0;
+  teststawka = 0;
+  poID('pstawka').innerHTML = stawka + "$";
+  konsola('Stawka została zresetowana!');
+}
+
+function zmianastawkix2(){
+  teststawka = stawka * 2;
+  if(teststawka > money){
+  konsola("Nie masz wystarczająco pieniędzy, żeby obstawić: " + teststawka + "$");
+  teststawka = stawka;
+  return;
+}
+else  {
+  stawka *= 2;
+  teststawka = stawka;
+  konsola("Obstawiles: " + stawka + "$");
+  poID('pstawka').innerHTML = stawka + "$";}
+}
+
+function zmianastawkiy2(){
+  teststawka = stawka / 2;
+  if(teststawka > money){
+  konsola("Nie masz wystarczająco pieniędzy, żeby obstawić: " + teststawka + "$");
+  teststawka = stawka;
+  return;
+}
+else  {
+  stawka /= 2;
+  teststawka = stawka;
+  konsola("Obstawiles: " + stawka + "$");
+  poID('pstawka').innerHTML = stawka + "$";}
+}
+function zmianastawkirandom(){
+stawka = (Math.floor(Math.random()*money+1));
+if(stawka > money){
+  stawka -= 1;
+  konsola('Nie mozesz ustawic losowej kwoty!');
+  return;
+}
+poID('pstawka').innerHTML = stawka + "$";
+konsola('Ustawiles losową stawkę tzn: ' + stawka + "$");
+  teststawka = 0;
+}
+
+function cfgraj(){
+if(obstawienie == null || stawka == 0){
+  konsola('Musisz ustawić swój typ oraz stawkę!');
+  return;
+}
+if(obstawieniekomputer == 1){
+  poID('obstawienie').innerHTML = "kx";}
+else{
+  poID('obstawienie').innerHTML = "jx";}
+
+obstawieniekomputer = (Math.floor(Math.random()*2));
+if(obstawieniekomputer == obstawienie){
+  konsola('Wygrales!');
+  poID('obstawienie').style = "color: green";
+  money += stawka;
+}
+if(obstawieniekomputer !== obstawienie){
+  konsola("Przegrales!");
+  poID('obstawienie').style = "color: red";
+  money -= stawka;
+}
+poID('pln').innerHTML = money + "$";
+konsola("Aktualny stan konta: " + money + "$");
+// zeby kuraw na minus nie bylo
+if(stawka > money){
+  stawka = money;
+  poID('pstawka').innerHTML = stawka + "$";
+  return;
+}
 }
