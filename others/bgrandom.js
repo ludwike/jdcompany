c709d268b1aaa339092711a6a19f69e8ff912b96
Ludwike <adam9871337@gmail.com>
2019-11-28 00:33:29 +0100

    s

--- others/bgrandom.js
+++ others/bgrandom.js
@@ -1,6 +1,10 @@
 var r = Math.floor(Math.random()*255+1);
 var g = Math.floor(Math.random()*255+1);
 var b = Math.floor(Math.random()*255+1);
+var rr;
+var gg;
+var bb;
+var x;
 var wspol = 4;
 var io1 = 0;
 var io2 = 0;
@@ -28,6 +32,31 @@ function tlo(){
       if(b>0){b -= wspol;}
       if(b<=0){io3 = 0}
     }
+rr = r;
+gg = g;
+bb = b;
+x = Round(Math.random()*2,2);
+while(x < 1.2 && x > 0.8){
+x = Round(Math.random()*2,2);
+}
+//rr *= x;
+//gg *= x;
+//bb *= x;
+//rr = Round(rr,2);
+//gg = Round(gg,2);
+//bb = Round(bb,2);
+if(rr > 255){rr = 0;}
+//if(gg > 255){gg = 0;}
+//if(bb > 255){bb = 0;}
+console.log(x);
 console.log(r + " " + g + " " + b);
+console.log(rr + " " + gg + " " + bb);
+console.log('-------------------');
 document.getElementById('b1').style.background = "rgb(" +r + "," +g + ","+b+ ")";
+//document.querySelectorAll('a').forEach(e => e.style.color = "rgb(" + rr + "," + gg + ","+ bb + ")");
+}
+function Round(n, k)
+{
+    var factor = Math.pow(10, k);
+    return Math.round(n*factor)/factor;
 }
